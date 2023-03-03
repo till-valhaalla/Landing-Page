fb26ac5bb5e98f1aec156bc69bca30ff86bf3d12
unknown <fatimakhalidipod@live.com>
2023-03-03 11:21:24 +0500

    body-content8

--- src/app/MyComponents/body-content8/body-content8.component.ts
+++ src/app/MyComponents/body-content8/body-content8.component.ts
@@ -1,10 +1,57 @@
 import { Component } from '@angular/core';
-
+interface Card {
+  imgurl1: string;
+  imgurl2: string;
+  title: string;
+  desc: string;
+  content: string;
+}
 @Component({
   selector: 'app-body-content8',
   templateUrl: './body-content8.component.html',
-  styleUrls: ['./body-content8.component.css']
+  styleUrls: ['./body-content8.component.css'],
 })
 export class BodyContent8Component {
-
+  cardData: Card[] = [
+    {
+      imgurl1: '../../../assets/quote.svg',
+      imgurl2: '../../../assets/People1.png',
+      title: 'Lorem Ipsum',
+      desc: 'Lorem ipsum dolor sit',
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Integer enim.',
+    },
+    {
+      imgurl1: '../../../assets/quote.svg',
+      imgurl2: '../../../assets/People2.png',
+      title: 'Lorem Ipsum',
+      desc: 'Lorem ipsum dolor sit',
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+    },
+    {
+      imgurl1: '../../../assets/quote.svg',
+      imgurl2: '../../../assets/People3.png',
+      title: 'Lorem Ipsum',
+      desc: 'Lorem ipsum dolor sit',
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+    {
+      imgurl1: '../../../assets/quote.svg',
+      imgurl2: '../../../assets/People4.png',
+      title: 'Lorem Ipsum',
+      desc: 'Lorem ipsum dolor sit',
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+    {
+      imgurl1: '../../../assets/quote.svg',
+      imgurl2: '../../../assets/People5.png',
+      title: 'Lorem Ipsum',
+      desc: 'Lorem ipsum dolor sit',
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore!',
+    },
+  ];
 }
