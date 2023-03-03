70612c0876b157e6cca153bdf21fa66a4775360f
unknown <fatimakhalidipod@live.com>
2023-03-03 16:02:07 +0500

    body-content9

--- src/app/MyComponents/body-content6/body-content6.component.ts
+++ src/app/MyComponents/body-content6/body-content6.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { faPlay } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-body-content6',
   templateUrl: './body-content6.component.html',
   styleUrls: ['./body-content6.component.css'],
 })
-export class BodyContent6Component {
-  faplayIcon = faPlay;
-}
+export class BodyContent6Component {}
--- src/app/MyComponents/body-content9/body-content9.component.ts
+++ src/app/MyComponents/body-content9/body-content9.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-body-content9',
   templateUrl: './body-content9.component.html',
-  styleUrls: ['./body-content9.component.css']
+  styleUrls: ['./body-content9.component.css'],
 })
-export class BodyContent9Component {
-
-}
+export class BodyContent9Component {}
