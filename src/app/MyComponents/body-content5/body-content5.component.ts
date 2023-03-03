fd07dea1c4b46c4a991b57656cd03c6e6fc98a80
unknown <fatimakhalidipod@live.com>
2023-03-03 09:33:49 +0500

    body-content5

--- src/app/MyComponents/body-content5/body-content5.component.ts
+++ src/app/MyComponents/body-content5/body-content5.component.ts
@@ -1,10 +1,59 @@
 import { Component } from '@angular/core';
-
+interface Card {
+  title: string;
+  imgUrl: string;
+  desc: string;
+  imgUrl2: string;
+  srno: string;
+}
 @Component({
   selector: 'app-body-content5',
   templateUrl: './body-content5.component.html',
-  styleUrls: ['./body-content5.component.css']
+  styleUrls: ['./body-content5.component.css'],
 })
 export class BodyContent5Component {
-
+  cardData: Card[] = [
+    {
+      title: 'Lorem Ipsum',
+      imgUrl: '../../../assets/card01.png',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.',
+      imgUrl2: '../../../assets/next.png',
+      srno: 'STEP 1',
+    },
+    {
+      title: 'Lorem Ipsum',
+      imgUrl: '../../../assets/card02.png',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      imgUrl2: '../../../assets/next.png',
+      srno: 'STEP 2',
+    },
+    {
+      title: 'Lorem Ipsum',
+      imgUrl: '../../../assets/card03.png',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.',
+      imgUrl2: '../../../assets/next.png',
+      srno: 'STEP 3',
+    },
+    {
+      title: 'Lorem Ipsum',
+      imgUrl: '../../../assets/card04.png',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      imgUrl2: '../../../assets/next.png',
+      srno: 'STEP 4',
+    },
+    {
+      title: 'Lorem Ipsum',
+      imgUrl: '../../../assets/card05.png',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.',
+      imgUrl2: '../../../assets/next.png',
+      srno: 'STEP 5',
+    },
+    {
+      title: 'Lorem Ipsum',
+      imgUrl: '../../../assets/card06.png',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer enim neque volutpat ac tincidunt.',
+      imgUrl2: '../../../assets/next.png',
+      srno: 'STEP 6',
+    },
+  ];
 }
