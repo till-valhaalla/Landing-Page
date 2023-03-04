9902ae011d3f74017c59bbbd9a45c0725223eed8
unknown <fatimakhalidipod@live.com>
2023-03-04 09:25:36 +0500

    changed style for body content 4

--- src/app/MyComponents/hero-content/hero-content.component.ts
+++ src/app/MyComponents/hero-content/hero-content.component.ts
@@ -1,37 +1,32 @@
 import { Component } from '@angular/core';
 import { faPlay } from '@fortawesome/free-solid-svg-icons';
-interface Card {
-  title: string;
-  bgcolor: string;
-  clicked: boolean;
-}
+
 @Component({
   selector: 'app-hero-content',
   templateUrl: './hero-content.component.html',
   styleUrls: ['./hero-content.component.css'],
 })
 export class HeroContentComponent {
-  cardData: Card[] = [
-    {
-      title: 'Lorem ipsum',
-      bgcolor: '#6184D8',
-      clicked: false,
-    },
-    {
-      title: 'Lorem ',
-      bgcolor: '#316EFF',
-      clicked: false,
-    },
-    {
-      title: 'Lorem Ipsum',
-      bgcolor: '#E86F3A',
-      clicked: false,
-    },
-    {
-      title: 'Lorem Ipsum Sit',
-      bgcolor: 'FFFFFF',
-      clicked: false,
-    },
-  ];
+  buttonText = 'Lorem ipsum';
+  texts = ['Lorem ipsum', 'Lorem ', 'Lorem Ipsum Sit'];
+  bgColor = '#6184D8';
+  bg = ['#23CE6B', '#E86F3A', '#316EFF'];
+  currentTextIndex = 0;
+
+  changeText() {
+    this.currentTextIndex++;
+    if (this.currentTextIndex >= this.texts.length) {
+      this.currentTextIndex = 0;
+    }
+    this.buttonText = this.texts[this.currentTextIndex];
+  }
+  changeBg() {
+    this.currentTextIndex++;
+    if (this.currentTextIndex >= this.bg.length) {
+      this.currentTextIndex = 0;
+    }
+    this.bgColor = this.bg[this.currentTextIndex];
+  }
+
   faplayIcon = faPlay;
 }
